ec9c85a149a6b9df8b96e554935317dceb3bc012
Maxim Gavrilove <maximgav123@gmail.com>
2024-06-03 16:51:11 +0300

    Commented HTTP Exceptions

--- tools/utils/access_token_utils.py
+++ tools/utils/access_token_utils.py
@@ -23,16 +23,17 @@ class AccessTokenUtils:
 
     # Decode an access token
     def decode_access_token(self, token: str):
-        try:
-            decoded_token = jwt.decode(jwt=token, key=JWT_SECRET, algorithms=JWT_ALGORITHM)
-            return decoded_token
-        except jwt.InvalidTokenError as error:
-            logging.error(str(error))
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail={"Error": "Invalid Access token"})
-        except Exception as error:
-            logging.error(str(error))
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail={"Error": "Internal Server Error"})
+        # try:
+        decoded_token = jwt.decode(jwt=token, key=JWT_SECRET, algorithms=JWT_ALGORITHM)
+        return decoded_token
+
+    # except jwt.InvalidTokenError as error:
+    #     logging.error(str(error))
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail={"Error": "Invalid Access token"})
+    # except Exception as error:
+    #     logging.error(str(error))
+    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    #                         detail={"Error": "Internal Server Error"})
 
     # Get the access token according to the client type
     def get_access_token_from_cookies(self, request: Request):
@@ -40,22 +41,23 @@ class AccessTokenUtils:
 
     # Return the access token data
     def get_access_token_data(self, request: Request):
-        try:
-            access_token = self.get_access_token_from_cookies(request)
+        # try:
+        access_token = self.get_access_token_from_cookies(request)
 
-            if access_token is None:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                    detail={"Error": "Access token not found"})
+        if access_token is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail={"Error": "Access token not found"})
 
-            decoded_access_token = self.decode_access_token(access_token)
-            return decoded_access_token
-        except HTTPException as error:
-            logging.error(str(error))
-            raise error
-        except Exception as error:
-            logging.error(str(error))
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail={"Error": "Internal Server Error"})
+        decoded_access_token = self.decode_access_token(access_token)
+        return decoded_access_token
+
+    # except HTTPException as error:
+    #     logging.error(str(error))
+    #     raise error
+    # except Exception as error:
+    #     logging.error(str(error))
+    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    #                         detail={"Error": "Internal Server Error"})
 
     # Set the access token in the response cookies
     def set_access_token_in_cookies(self, response: Response, access_token: str):
