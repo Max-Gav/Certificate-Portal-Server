8e06c167b59ddba25dbffb72e85250f648d9d3ea
Maxim Gavrilove <maximgav123@gmail.com>
2024-06-03 14:21:47 +0300

    Commented HTTP Exceptions raises

--- tools/utils/password_utils.py
+++ tools/utils/password_utils.py
@@ -16,44 +16,46 @@ class PasswordUtils:
 
     # Decode password from base64
     def decode_password_from_base64(self, password):
-        try:
-            return base64.b64decode(password).decode('utf-8')
-        except Exception as error:
-            logging.error(str(error))
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                detail={"Error": "Password is not using the right base64 encoding"})
+        # try: 
+        return base64.b64decode(password).decode('utf-8')
 
-    # Encrypt a password using hash
-    async def encrypt_password(self, password: str) -> str:
-        try:
-            decoded_password = self.decode_password_from_base64(password)
-            if not self.is_password_length_valid(decoded_password):
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"Error": "Invalid password"})
+    # except Exception as error:
+    #     logging.error(str(error))
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+    #                         detail={"Error": "Password is not using the right base64 encoding"})
 
-            peppered_password = decoded_password + self.pepper_secret
-            salt = await aiobcrypt.gensalt()
-            hashed_password = await aiobcrypt.hashpw(peppered_password.encode('utf-8'), salt)
+    # Encrypt a password using hash from base64
+    async def encrypt_base64_password(self, password: str) -> str:
+        # try:
+        decoded_password = self.decode_password_from_base64(password)
+        # if not self.is_password_length_valid(decoded_password):
+        #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"Error": "Invalid password"})
 
-            return hashed_password.decode('utf-8')
-        except HTTPException as error:
-            logging.error(str(error))
-            raise error
-        except Exception as error:
-            logging.error(str(error))
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail={"Error": "Internal Server Error"})
+        peppered_password = decoded_password + self.pepper_secret
+        salt = await aiobcrypt.gensalt()
+        hashed_password = await aiobcrypt.hashpw(peppered_password.encode('utf-8'), salt)
+
+        return hashed_password.decode('utf-8')
+
+    # except HTTPException as error:
+    #     logging.error(str(error))
+    #     raise error
+    # except Exception as error:
+    #     logging.error(str(error))
+    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    #                         detail={"Error": "Internal Server Error"})
 
     # Check if the provided password matches the hashed password
     async def compare_password(self, password: str, hashed_password: str) -> bool:
-        try:
-            decoded_password = self.decode_password_from_base64(password)
-            peppered_password = decoded_password + self.pepper_secret
+        # try:
+        decoded_password = self.decode_password_from_base64(password)
+        peppered_password = decoded_password + self.pepper_secret
 
-            return await aiobcrypt.checkpw(peppered_password.encode('utf-8'), hashed_password.encode('utf-8'))
-        except HTTPException as error:
-            logging.error(str(error))
-            raise error
-        except Exception as error:
-            logging.error(str(error))
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail={"Error": "Internal Server Error"})
+        return await aiobcrypt.checkpw(peppered_password.encode('utf-8'), hashed_password.encode('utf-8'))
+        # except HTTPException as error:
+        #     logging.error(str(error))
+        #     raise error
+        # except Exception as error:
+        #     logging.error(str(error))
+        #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        #                         detail={"Error": "Internal Server Error"})
