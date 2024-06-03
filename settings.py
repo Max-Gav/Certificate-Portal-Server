66aaea3985715531835ab11cf129c1ce44242ce9
Maxim Gavrilove <maximgav123@gmail.com>
2024-06-03 14:18:36 +0300

    Added CORS settings

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic_settings import BaseSettings
 
 
@@ -6,3 +8,7 @@ class Settings(BaseSettings):
     host: str = "127.0.0.1"
     port: int = 9000
     reload: bool = True
+    allow_origins: List[str] = ["http://localhost:5173"]
+    allow_methods: List[str] = ["*"]
+    allow_credentials: bool = True
+    allow_headers: List[str] = ["*"]
