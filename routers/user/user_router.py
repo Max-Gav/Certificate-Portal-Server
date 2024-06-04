3112a14eee220feaa86f9f0a3d669470de0c62ad
Maxim Gavrilove <maximgav123@gmail.com>
2024-06-04 13:47:07 +0300

    Fixed response cookies

--- routers/user/user_router.py
+++ routers/user/user_router.py
@@ -1,21 +1,19 @@
 from fastapi import APIRouter, Request, Response, status
 from models.user_route_models.user import User
 from routers.user.service import UserService
-from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/users")
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_user(request: Request, response: Response, user: User):
-    await UserService().login_check(response=response, user=user)
+    await UserService().login_user(response=response, user=user)
 
-    return JSONResponse(content={"message": "Successfully logged in."}, status_code=status.HTTP_200_OK)
+    return {"message": "Successfully logged in."}
 
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(request: Request, response: Response, user: User):
     await UserService().user_registration(response=response, user=user)
 
-    return JSONResponse(content={"message": "Successfully created an user."}, status_code=status.HTTP_201_CREATED)
-
+    return {"message": "Successfully created an user."},
