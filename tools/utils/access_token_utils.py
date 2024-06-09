a9fea798ffbd854ad014d1b56043fae0a2549dce
Maxim Gavrilove <maximgav123@gmail.com>
2024-06-09 13:52:33 +0300

    Removed Comments

--- tools/utils/access_token_utils.py
+++ tools/utils/access_token_utils.py
@@ -9,31 +9,26 @@ settings = Settings()
 JWT_ALGORITHM = settings.jwt_algorithm
 JWT_SECRET = os.getenv("JWT_SECRET")
 
-
+# Class that provides access token utilities for creating, encoding, etc.
 class AccessTokenUtils:
-    # Create a new token
-    def create_access_token(self, user_id: str, user_role: str) -> str:
-        payload = {
+    # Build a access token payload
+    def access_token_payload_builder(self, user_id: str, user_role: str, expiry_time: int):
+        return {
             "id": user_id,
             "role": user_role,
-            "expiry": time.time() + 600
+            "expiry": time.time() + expiry_time
         }
+
+    # Create a new token
+    def create_access_token(self, user_id: str, user_role: str) -> str:
+        payload = self.access_token_payload_builder(user_id, user_role, 600)
         token = jwt.encode(payload=payload, key=JWT_SECRET, algorithm=JWT_ALGORITHM)
         return token
 
     # Decode an access token
     def decode_access_token(self, token: str):
-        # try:
         decoded_token = jwt.decode(jwt=token, key=JWT_SECRET, algorithms=JWT_ALGORITHM)
         return decoded_token
-
-    # except jwt.InvalidTokenError as error:
-    #     logging.error(str(error))
-    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail={"Error": "Invalid Access token"})
-    # except Exception as error:
-    #     logging.error(str(error))
-    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-    #                         detail={"Error": "Internal Server Error"})
 
     # Get the access token according to the client type
     def get_access_token_from_cookies(self, request: Request):
@@ -50,14 +45,6 @@ class AccessTokenUtils:
 
         decoded_access_token = self.decode_access_token(token=access_token)
         return decoded_access_token
-
-    # except HTTPException as error:
-    #     logging.error(str(error))
-    #     raise error
-    # except Exception as error:
-    #     logging.error(str(error))
-    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-    #                         detail={"Error": "Internal Server Error"})
 
     # Set the access token in the response cookies
     def set_access_token_in_cookies(self, response: Response, access_token: str):
