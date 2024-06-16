583d23ff948540faae33b2d7850ba49066ab023f
Maxim Gavrilove <maximgav123@gmail.com>
2024-06-16 13:49:10 +0300

    Moved static and private methods outside of access token class

--- tools/utils/access_token_utils.py
+++ tools/utils/access_token_utils.py
@@ -15,6 +15,21 @@ EXPIRY_TIME_MINUTES = 30
 EXPIRY_TIME = EXPIRY_TIME_MINUTES * 60  # Seconds
 
 
+# Build an access token payload
+def access_token_payload_builder(user_id: str, user_role: str) -> TokenPayload:
+    return TokenPayload(
+        id=user_id,
+        role=user_role,
+        expiry=time.time() + EXPIRY_TIME
+    )
+
+
+# Decode an access token
+def decode_access_token(token: str):
+    decoded_token = jwt.decode(jwt=token, key=JWT_SECRET, algorithms=JWT_ALGORITHM)
+    return decoded_token
+
+
 # Class that provides access token utilities for creating, encoding, etc.
 class AccessTokenUtils:
     # Access token payload middleware
@@ -29,24 +44,11 @@ class AccessTokenUtils:
         except DecodeError:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Access token is invalid")
 
-    # Build an access token payload
-    def access_token_payload_builder(self, user_id: str, user_role: str) -> TokenPayload:
-        return TokenPayload(
-            id=user_id,
-            role=user_role,
-            expiry=time.time() + EXPIRY_TIME
-        )
-
     # Create a new token
     def create_access_token(self, user_id: str, user_role: str) -> str:
-        payload = self.access_token_payload_builder(user_id, user_role)
+        payload = access_token_payload_builder(user_id, user_role)
         token = jwt.encode(payload=dict(payload), key=JWT_SECRET, algorithm=JWT_ALGORITHM)
         return token
-
-    # Decode an access token
-    def decode_access_token(self, token: str):
-        decoded_token = jwt.decode(jwt=token, key=JWT_SECRET, algorithms=JWT_ALGORITHM)
-        return decoded_token
 
     # Get the access token according to the client type
     def get_access_token_from_cookies(self, request: Request) -> str:
@@ -59,7 +61,7 @@ class AccessTokenUtils:
         if access_token is None:
             return None
 
-        payload = self.decode_access_token(token=access_token)
+        payload = decode_access_token(token=access_token)
         return payload
 
     # Set the access token in the response cookies
