22e2252d2cf724143a42e7bf116c0d8280aefe47
hafifa1 <hiladvir14@gmail.com>
2024-06-16 16:00:58 +0300

    Added base user model and fixed user role in DB

--- models/user_route_models/user.py
+++ models/user_route_models/user.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 
 
-class User(BaseModel):
+class BaseUser(BaseModel):
     username: str
     password: str
+
+class User(BaseUser):
+    role: str
--- routers/user/user_service.py
+++ routers/user/user_service.py
@@ -1,7 +1,7 @@
 import binascii
 
 from routers.user.user_repo import UserRepo
-from models.user_route_models.user import User
+from models.user_route_models.user import User, BaseUser
 from tools.utils.password_utils import PasswordUtils
 from tools.utils.access_token_utils import AccessTokenUtils
 from fastapi import Response, HTTPException, status
@@ -22,6 +22,8 @@ class UserService:
     # Service for checking login details
     async def login_user(self, response: Response, user: User) -> bool:
         user_from_db = await self._repo.find_user_in_database(user)
+    async def login_user(self, response: Response, base_user: BaseUser) -> bool:
+        user_from_db = await self._repo.find_user_in_database(base_user)
 
         if user_from_db is None:
             raise HTTPException(
@@ -29,7 +31,7 @@ class UserService:
                 detail="User not found"
             )
         try:
-            is_same_password = await self.password_utils.compare_password(user.password, user_from_db.get("password"))
+            is_same_password = await self.password_utils.compare_password(base_user.password, user_from_db.get("password"))
         except (UnicodeDecodeError, binascii.Error):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password is not encoded using base64.")
         if not is_same_password:
@@ -45,11 +47,12 @@ class UserService:
         return True
 
     # Service for registering a new user to the database
-    async def register_user(self, response: Response, user: User) -> bool:
+    async def register_user(self, response: Response, base_user: BaseUser) -> bool:
         try:
-            user.password = await self.password_utils.encrypt_base64_password(user.password)
+            base_user.password = await self.password_utils.encrypt_base64_password(base_user.password)
         except (UnicodeDecodeError, binascii.Error):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password encoding is not valid.")
+        user = User(role="user", **(base_user.model_dump()))
         user_id = await self._repo.create_user_in_database(user)
 
         user_id = str(user_id)
