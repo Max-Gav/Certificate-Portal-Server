2f84ef1652318dafcb4e8a7029ddf3ad6f05681b
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-14 11:08:46 +0300

    Updated certificate models

--- models/certificate_route_models/certificate.py
+++ models/certificate_route_models/certificate.py
@@ -5,12 +5,23 @@ from datetime import datetime
 class BaseCertificate(BaseModel):
     cert_name: str
     common_name: str
-    subject_alternative_names: list[str]
+    country_name: str
+    state_or_province_name: str
+    locality_name: str
+    organization_name: str
+    organizational_unit_name: str
+    email_address: str
+    domain_names: list[str]
+    ip_addresses: list[str]
     expiration_date: datetime
 
 
 class Certificate(BaseCertificate):
     user_id: str
+
+
+class FullCertificate(Certificate):
+    certificate_id: str
 
 
 class EditCertificate(BaseModel):
