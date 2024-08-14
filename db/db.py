b4d72fc4fa4eca65fbcc9403d17d2ded3297b81e
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-14 11:20:35 +0300

    Implemented create certificate route

--- db/db.py
+++ db/db.py
@@ -1,13 +1,15 @@
 import logging
 
 from tools.singleton import Singleton
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase, AsyncIOMotorGridFSBucket
+
 
 class MongoConnector(metaclass=Singleton):
     mongodb_uri: str = None
     db_name: str = None
     mongodb_client: AsyncIOMotorClient = None
     db: AsyncIOMotorDatabase = None
+    fs: AsyncIOMotorGridFSBucket = None
 
     def init(self, mongodb_uri, db_name) -> None:
         self.mongodb_uri = mongodb_uri
@@ -18,6 +20,9 @@ class MongoConnector(metaclass=Singleton):
 
         self.db = self.mongodb_client[self.db_name]
         logging.info("Accessing the database named: " + self.db_name)
+
+        self.fs = AsyncIOMotorGridFSBucket(self.db)
+        logging.info("Initialized GridFS")
 
     def close_connection(self):
         self.mongodb_client.close()
