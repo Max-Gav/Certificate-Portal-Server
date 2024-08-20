e7d41ed9014b21409cf32f8b382469c645dec128
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-20 22:08:44 +0300

    Added upload certificate service endpoint path constant

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 class Config:
     CERTIFICATE_OPERATIONS_SERVICE_URL = os.getenv("CERTIFICATE_OPERATIONS_SERVICE_URL", "http://127.0.0.1:9200")
     CREATE_CERTIFICATE_ENDPOINT = "/cert-ops/create-certificate"
+    UPLOAD_CERTIFICATE_ENDPOINT = "/cert-ops/upload-certificate"
