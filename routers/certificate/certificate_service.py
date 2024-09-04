40af5ed7a7a25683aa4d81928a714c647c5c13a3
Maxim Gavrilove <maximgav123@gmail.com>
2024-09-04 20:27:37 +0300

    Imported service endpoints

--- routers/certificate/certificate_service.py
+++ routers/certificate/certificate_service.py
@@ -10,8 +10,7 @@ from models.common.token_payload import TokenPayload
 from routers.certificate.certificate_repo import CertificateRepo
 from tools.utils.access_token_utils import AccessTokenUtils
 from bson.objectid import ObjectId
-from config import Config
-
+from config.service_endpoints import ServiceEndpoints
 from settings import settings
 
 
@@ -60,7 +59,7 @@ class CertificateService:
         certificate_data_user_id = CertificateDataUserId(user_id=payload["id"], **(certificate_data.model_dump()))
         certificate_object_id = await self._repo.create_certificate(certificate_data_user_id=certificate_data_user_id)
 
-        create_certificate_url = self.certificate_operations_service_url + Config.CREATE_CERTIFICATE_ENDPOINT
+        create_certificate_url = self.certificate_operations_service_url + ServiceEndpoints.CREATE_CERTIFICATE_ENDPOINT
 
         full_certificate_data = FullCertificateData(certificate_id=str(certificate_object_id),
                                                     **(certificate_data_user_id.model_dump()))
@@ -73,7 +72,7 @@ class CertificateService:
                                  certificate_file: UploadFile,
                                  background_tasks: BackgroundTasks,
                                  payload: TokenPayload) -> None:
-        upload_certificate_url = self.certificate_operations_service_url + Config.UPLOAD_CERTIFICATE_ENDPOINT
+        upload_certificate_url = self.certificate_operations_service_url + ServiceEndpoints.UPLOAD_CERTIFICATE_ENDPOINT
 
         certificate_upload_details = UploadCertificate(user_id=payload["id"], cert_name=cert_name)
         certificate_file_content = await certificate_file.read()
