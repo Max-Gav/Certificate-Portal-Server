09651bb04b6fbe9baf5b846b3a245e0f4d327feb
Maxim Gavrilove <maximgav123@gmail.com>
2024-09-04 20:22:09 +0300

    Sorted settings.py file

--- settings.py
+++ settings.py
@@ -10,8 +10,15 @@ class Settings(BaseSettings):
     host: str = "127.0.0.1" if is_dev else "0.0.0.0"
     port: int = 9000
     reload: bool = False
+
     allow_origins: List[str] = ["http://localhost:5173"] if is_dev else ["http://localhost:5173","http://127.0.0.1:5173", "http://0.0.0.0:5173","http://localhost:8080", "*"]
     allow_methods: List[str] = ["*"]
     allow_credentials: bool = True
     allow_headers: List[str] = ["*"]
+
     jwt_algorithm: str = "HS256"
+
+    CERTIFICATE_OPERATIONS_SERVICE_URL:str = os.getenv("CERTIFICATE_OPERATIONS_SERVICE_URL", "http://127.0.0.1:9200")
+
+
+settings = Settings()
